861d7c99ab63a3d0b04ff3e53ae8e35caf5eae63
Aviran Avraham Swissa <aviran.swissa@gmail.com>
2019-12-12 14:35:37 +0200

    update gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAjTaoGfiizD4uGEHO3jLohQ3uQ8-lg2-c",
-    authDomain: "todo-ce834.firebaseapp.com",
-    databaseURL: "https://todo-ce834.firebaseio.com",
-    projectId: "todo-ce834",
-    storageBucket: "todo-ce834.appspot.com",
-    messagingSenderId: "159502353977",
-    appId: "1:159502353977:web:dfb8494ee1db13f09f6fe8"
+
   }
 };
 
